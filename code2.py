05ec2973d0471ddc18b2e517a12b3cdcdc3001a2
P1raveen4 <piraveena-yoganathan@wilder.school>
2024-09-27 20:22:17 +0200

    Update code2.py

--- code2.py
+++ code2.py
@@ -99,7 +99,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Insertion du logo
-st.image('enedis.png', width=120)
+st.image('logo PY²MN.png', width=120)
 
 # Titre principal de la page
 st.markdown("<h1>Tableau de bord énergétique - Enedis</h1>", unsafe_allow_html=True)

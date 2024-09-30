f39bc031388a3b90d4b5a129161a5600ba1de27e
P1raveen4 <piraveena-yoganathan@wilder.school>
2024-09-30 15:35:03 +0200

    Update code2.py

--- code2.py
+++ code2.py
@@ -347,13 +347,6 @@ elif section == "Section 3 : Prédiction basée sur données historiques":
     ### Explication :
     Cette section vous permet de prédire la consommation énergétique à partir de données historiques (température, précipitations, etc.) en utilisant un modèle d'apprentissage automatique de type Random Forest.
     """)
-
-
-
-     st.markdown("""
-    ### Explication :
-    coucou
-    """)
     def train_model(X, y):
         # Standardisation et imputation
         scaler_X = StandardScaler()
